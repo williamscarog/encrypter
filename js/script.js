460ef67e102624cb59e53afd59f0984eba79a338
Williamscarog <williamscaro194@gmail.com>
2024-08-27 22:29:48 -0400

    Ya funcional, trabajando para mejorar la encriptacion del texto con js.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,64 @@
 let textArea = document.querySelector(".arialText");
 let arialText = document.querySelector(".arialText-clipboard");
 
-function encrypt() {
+
+function btnEncrypt() {
+  const encryptText = encrypt(textArea.value)
+  arialText.value = encryptText
+  textArea.value = "";
+  arialText.style.backgroundImage = "none";
+
+}
+function btnDecrypt() {
+  const encryptText = uncrypt(arialText.value)
+  textArea.value = encryptText
+  arialText.value = "";
 
 
 }
+
+function encrypt(encriptString) {
+  const codigosMatriz = [
+    //0  & 1 Esté es el orden de las posiciones.
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+  ];
+
+
+  encriptString = encriptString.toLowerCase();
+  for (let index = 0; index < codigosMatriz.length; index++) {
+    if (encriptString.includes(codigosMatriz[index][0])) {
+      encriptString = encriptString.replaceAll(codigosMatriz[index][0], codigosMatriz[index][1])
+
+    }
+
+  }
+  return encriptString;
+
+};
+
+function uncrypt(uncriptString) {
+  const codigosMatriz = [
+    //0  & 1 Esté es el orden de las posiciones.
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"]
+  ];
+
+
+  uncriptString = uncriptString.toLowerCase();
+  for (let index = 0; index < codigosMatriz.length; index++) {
+    if (uncriptString.includes(codigosMatriz[index][1])) {
+      uncriptString = uncriptString.replaceAll(codigosMatriz[index][1], codigosMatriz[index][0])
+
+    }
+
+  }
+  return uncriptString;
+
+};
